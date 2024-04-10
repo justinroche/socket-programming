de8914bb50cc4b8c698f21e84cf158fde2ba393a
justinroche <justinroche03@gmail.com>
2024-04-10 12:02:35 -0500

    Implement steps 1 and 2 (no encryption yet)

--- client/Client.cs
+++ client/Client.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace SocketProgram
+{
+  class Client
+  {
+
+    static readonly int port = 11000;
+    static readonly string ip = "127.0.0.1";
+
+    static void Main(string[] args)
+    {
+      Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Initialize the sender.
+      sender.Connect(ip, port); // Connect sender to the IP and port.
+
+      // Prepare message to be sent.
+      Console.WriteLine("Enter a message to send to the server:");
+      string message = Console.ReadLine() ?? string.Empty;
+      byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+
+      // TODO: Encrypt the message.
+
+      sender.Send(messageBytes); // Send the message to the server.
+      Console.WriteLine("Message sent to the server.");
+      Console.WriteLine("Waiting for response...");
+
+      // Receive the response from the server.
+      byte[] buffer = new byte[1024];
+      int responseBytes = sender.Receive(buffer);
+
+      // TODO: Decrypt the response.
+
+      // Convert the response to a string and print it.
+      string response = Encoding.ASCII.GetString(buffer, 0, responseBytes);
+      Console.WriteLine("Response from server: " + response);
+
+      // Shutdown and close the sender.
+      sender.Shutdown(SocketShutdown.Both);
+      sender.Close();
+    }
+  }
+}
--- server/Server.cs
+++ server/Server.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketProgram
+{
+  class Server
+  {
+
+    static readonly string ip = "127.0.0.1";
+    static readonly int port = 11000;
+
+    static void Main(string[] args)
+    {
+      Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Initialize the listener.
+      listener.Bind(new IPEndPoint(IPAddress.Parse(ip), port)); // Bind the listener to the IP and port.
+
+      // Listen for incoming connections.
+      listener.Listen(1);
+      Console.WriteLine("Server listening on " + ip + ":" + port + "...");
+
+      while (true)
+      {
+        Socket handler = listener.Accept(); // Accept a new connection and get the client socket.
+
+        // Receive the message from the client.
+        byte[] buffer = new byte[1024];
+        handler.Receive(buffer);
+
+        // TODO: Decrypt the message.
+
+        string request = Encoding.UTF8.GetString(buffer).TrimEnd('\0');
+        Console.WriteLine("Received " + request);
+
+        // Convert the message to uppercase and send it back.
+        string response = request.ToUpper();
+        byte[] responseBuffer = Encoding.UTF8.GetBytes(response);
+
+        // TODO: Encrypt the response.
+
+        handler.Send(responseBuffer);
+
+        // Shutdown and close the handler.
+        handler.Shutdown(SocketShutdown.Both);
+        handler.Close();
+      }
+
+    }
+  }
+}
